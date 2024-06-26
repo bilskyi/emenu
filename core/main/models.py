cba0471941b71f3d57e9013718c32487ad32acbb
bilskyi <bilskyioleksandr@outlook.com>
2024-06-26 13:52:02 +0300

    adding base models

--- core/main/models.py
+++ core/main/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    dishes = models.ManyToManyField('MenuItem')
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    photo = models.ImageField(upload_to='media')
+    price = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+ 
+    def __str__(self) -> str:
+        return self.name
+        
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
